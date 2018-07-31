10ee8abdba51a339df6cddd44f226cb8662a7945
Russ Thompson <vigornj@gmail.com>
2018-07-31 17:27:00 -0400

    Use event ID and count
    Use the EventID and the Count so that only new/updated events send alerts.

--- aws-guardduty-notifications.sh
+++ aws-guardduty-notifications.sh
@@ -28,10 +28,6 @@ CHECK_INTERVAL="60"
 # You can set this to an instance ID, security group, etc.  Leave default setting to disable
 IGNORE_INSTANCE="i-123456789abc"
 
-# In AWS GuardDuty, it's possible to archive events for a period of time.  When the archival expires, those events resurface.
-# By setting this to "1" we only send events that match todays date (UTC).  Setting to '0' sends all events regardless of age.
-IGNORE_OLD_EVENTS="1"
-
 ### No need to edit anything below this line.
 
 readonly BASE_NAME=$(basename -- "$0")
@@ -43,11 +39,13 @@ touch $EVENT_STORAGE_FILE
 function send_mm_alert(){
   if [[ ! "$(echo $EVENT_DATA)" == *"$IGNORE_INSTANCE"* ]] ; then
     if [ $GD_SEVERITY -gt $MAJOR_ALERT_SEVERITY ] ; then
-      curl -s -i -X POST -H 'Content-Type: application/json' -d '{"text": "** <!channel> '"$GD_MESSAGE"' ** [`'"$GD_INSTANCE_TAG"'`]\n * Event: UID='$GD_EVENT_ID' | Time='$GD_EVENT_TIME' | Count='$GD_COUNT' | Severity='$GD_SEVERITY' | Source=GuardDuty"}' \
+      curl -s -i -X POST -H 'Content-Type: application/json' -d \
+      '{"username": "guardduty-bot", "text": "** <!channel> '"$GD_MESSAGE"' ** [`'"$GD_INSTANCE_TAG"'`]\n * Event: UID='$GD_EVENT_ID' | Time='$GD_EVENT_TIME' | Count='$GD_COUNT' | Severity='$GD_SEVERITY' | Source=GuardDuty"}' \
       $WEB_HOOK_URL
       logger -t INFO "$BASE_NAME [alert-notable]:  $GD_MESSAGE UID=$GD_EVENT_ID"
     else
-      curl -s -i -X POST -H 'Content-Type: application/json' -d '{"text": "** Alert: '"$GD_MESSAGE"' ** [`'"$GD_INSTANCE_TAG"'`]\n * Event: UID='$GD_EVENT_ID' | Time='$GD_EVENT_TIME' | Count='$GD_COUNT' | Severity='$GD_SEVERITY' | Source=GuardDuty"}' \
+      curl -s -i -X POST -H 'Content-Type: application/json' -d \
+      '{"username": "guardduty-bot", "text": "** Alert: '"$GD_MESSAGE"' ** [`'"$GD_INSTANCE_TAG"'`]\n * Event: UID='$GD_EVENT_ID' | Time='$GD_EVENT_TIME' | Count='$GD_COUNT' | Severity='$GD_SEVERITY' | Source=GuardDuty"}' \
       $WEB_HOOK_URL
       logger -t INFO "$BASE_NAME [alert-basic]:  $GD_MESSAGE UID=$GD_EVENT_ID"
     fi      
@@ -56,7 +54,9 @@ function send_mm_alert(){
     logger -t WARN  "$BASE_NAME [alert-ignored]:  $GD_MESSAGE UID=$GD_EVENT_ID"
   fi
   EVENT_DATA=""
-  echo $gd_events >> $EVENT_STORAGE_FILE
+  # Remove existing events with the same UID and different count
+  sed -i "/$gd_events/d" $EVENT_STORAGE_FILE
+  echo $GD_EVENT_AND_COUNT >> $EVENT_STORAGE_FILE
 }
 
 # FUNCTION (parse_event_data):  Parse the event JSON data with the jq utility, place useful data into variables.
@@ -64,22 +64,15 @@ function parse_event_data(){
   GD_MESSAGE="$(echo $EVENT_DATA | jq -r .Findings[].Title)"
   GD_COUNT="$(echo $EVENT_DATA | jq -r .Findings[].Service.Count?)"
   GD_SEVERITY="$(echo $EVENT_DATA | jq -r .Findings[].Severity)"
+  # eventLastSeen
   GD_EVENT_TIME="$(echo $EVENT_DATA | jq -r .Findings[].Service.EventFirstSeen?)"
   GD_EVENT_ID="$(echo $EVENT_DATA | jq -r .Findings[].Id)"
   GD_INSTANCE_TAG="$(echo $EVENT_DATA | jq -r '.Findings[].Resource[].Tags? | map(select(.Key == '\"$INSTANCE_TAG\"')) | .[].Value')"
-}
+  GD_EVENT_AND_COUNT="$gd_events:$GD_COUNT"
+  # Instance details aren't provided on user activity, grab username field (if available)
+  if [ -z "$GD_INSTANCE_TAG" ] ; then GD_INSTANCE_TAG="null" ; fi
 
-# FUNCTION (check_event_date):  Check the date of the event and call alert function (or not, conditional)
-function check_event_date(){
-  if [ "$IGNORE_OLD_EVENTS" == "1" ] ; then
-    if [[ "$GD_EVENT_TIME" == *"$(date +%Y-%m-%d)"* ]] ; then
-      send_mm_alert
-    else
-      # It's an old event thats archival expired.
-      EVENT_DATA=""
-      echo $gd_events >> $EVENT_STORAGE_FILE
-    fi
-  else
+  if ! grep -w $GD_EVENT_AND_COUNT $EVENT_STORAGE_FILE ; then 
     send_mm_alert
   fi
 }
@@ -88,14 +81,9 @@ function check_event_date(){
 while true ; do
   for gd_events in `aws guardduty list-findings --detector-id $DIRECTOR_ID | jq -r .FindingIds[]` ; do
     # Check our events file to see if this is a new event or old.
-    if ! grep $gd_events $EVENT_STORAGE_FILE ; then
       EVENT_DATA="$(aws guardduty get-findings --detector-id $DIRECTOR_ID --finding-ids $gd_events)"
       # Parse the data of any events discovered
       parse_event_data 
-      # Check the date of the event and then call alert to be sent (or not)
-      check_event_date
-    fi
   done
   sleep $CHECK_INTERVAL
 done
-
